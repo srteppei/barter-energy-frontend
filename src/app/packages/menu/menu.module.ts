0f2d810c437bc8a22d7fb297c904bba59c8d8785
sr.teppei <jesus.hernanz.mateo@gmail.com>
2021-01-31 15:01:12 +0000

    Create tool-bar

---
+++ src/app/packages/menu/menu.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { NavBarComponent } from './component/nav-bar/nav-bar.component';
+import { ToolBarComponent } from './component/tool-bar/tool-bar.component';
+
+
+
+@NgModule({
+  declarations: [NavBarComponent, ToolBarComponent],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatMenuModule,
+    MatIconModule,
+  ],
+  exports: [ToolBarComponent]
+})
+export class MenuModule { }
